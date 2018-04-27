b6ba0a43861286ab8922834a6492fda3d3d0798a
JianningRuan <675690521@qq.com>
2018-04-27 22:48:51 +0800

    基础

--- Router/user.js
+++ Router/user.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/info', function (req, res) {
-        res.json({'code': 1})
+        res.json({code: 0})
+});
+
+router.get('/login', function (req, res) {
+        res.json({code: 1})
 });
 
 module.exports = router;
