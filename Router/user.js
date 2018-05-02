8f885866ea4b564a208dbac1e7485067715ac61d
ruanjianning <ruanjianning@qtone.cn>
2018-05-02 18:10:32 +0800

    基础

--- Router/user.js
+++ Router/user.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/info', function (req, res) {
         res.json({code: 0})
 });
 
 router.get('/login', function (req, res) {
         res.json({code: 1})
+});
+
+router.post('/register', function (req, res) {
+    res.json({code: 1})
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
+
+const mongodb = require('mongodb');
 const user = require('./Router/user');
 
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use('/user', user);
 
 const server = app.listen(9093, function () {
         const host = server.address().address;
         const port = server.address().port;
-        console.log('App listening at http://%s:%s', host, port);
+        console.log('App listening at http://', host, port);
 })
