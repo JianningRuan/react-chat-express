d4c8941bb01dda02c03004369734a7b6264f1267
ruanjianning <ruanjianning@qtone.cn>
2018-05-16 18:02:16 +0800

    基础

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-/* const mongoose = require('mongoose');
-const DB_URL = 'mongodb://localhost:27017/react-chat';
-mongoose.connect(DB_URL, function (err, db) {
-    console.log(err);
-    console.log(db);
-}); */
+// socket with express
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const user = require('./Router/user');
 
@@ -16,8 +13,17 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use('/user', user);
 
-const server = app.listen(9093, function () {
+server.listen(9093, function () {
     const host = server.address().address;
     const port = server.address().port;
     console.log('App listening at http://', host, port);
+});
+
+// socket
+io.on('connection', (socket)=>{
+    console.log('链接上了');
+    socket.on('aa', (data)=>{
+        console.log(data);
+        io.emit('se', data);
+    })
 });
