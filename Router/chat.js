75143a0e59a7f805be91ff39e36612236c5794f3
ruanjianning <ruanjianning@qtone.cn>
2018-05-18 18:02:29 +0800

    基础

--- Router/chat.js
+++ Router/chat.js
@@ -7,14 +7,22 @@ const User = model.getModel('user');
 router.get('/getMsgList', function (req, res) {
     const userId = req.cookies.userId;
     User.find({}, function (userErr, userDoc) {
+        console.log('获取：', userDoc);
+        let users = {};
+        userDoc.forEach(v=>{
+            users[v._id] = {name: v.user, headPic: v.headPic}
+        });
+        console.log(users);
+
+        Chat.find({$or: [{from: userId}, {to: userId}]}, function (err, doc) {
+            if (err){
+                return res.json({code: 0, errMsg: '数据出错'})
+            }
+            res.json({code: 1, data: doc, users: users})
+        })
 
     });
-    Chat.find({$or: [{from: userId}, {to: userId}]}, function (err, doc) {
-        if (err){
-            return res.json({code: 0, errMsg: '数据出错'})
-        }
-        res.json({code: 1, data: doc})
-    })
+
 });
 
 module.exports = router;
