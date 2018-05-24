ba6a20079d1de142e20a42d46c3150db15f1b996
ruanjianning <ruanjianning@qtone.cn>
2018-05-24 17:55:53 +0800

    基础

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 
 // 跨域设置请求头
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-File-Name");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header('Cache-Control', 'no-store');
     next();
 });
+/*app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});*/
+/*app.use(cors({
+    origin:['http://localhost:3000'],
+    methods:['GET','POST'],
+    alloweHeaders:['Content-Type', 'Authorization']
+}));*/
 
 // 链接数据库
 const model = require('./model');
